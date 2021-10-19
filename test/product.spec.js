653e9914e3a22af6c1e62bef482d2663e6f5df51
Anton Kjus <antonkjus@gmail.com>
2021-10-19 11:42:02 +0200

    Nå kan du skrive tester til api Lagd en enkel test som forventer at det er 10 producter i http://localhost:3000/products

---
+++ test/product.spec.js
@@ -0,0 +1,28 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+
+chai.should()
+
+chai.use(chaiHttp)
+
+// eslint-disable-next-line no-undef
+describe('Product API', () => {
+  /**
+   * Test GET Route
+   */
+  // eslint-disable-next-line no-undef
+  describe('GET products', () => {
+    // eslint-disable-next-line no-undef
+    it('it should GET all products', (done) => {
+      chai.request('http://localhost:3000')
+        .get('/products')
+        // eslint-disable-next-line handle-callback-err
+        .end((err, response) => {
+          response.should.have.status(200)
+          response.body.should.be.a('array')
+          response.body.length.should.be.eq(10)
+          done()
+        })
+    })
+  })
+})
