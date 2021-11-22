7f7710f93af6fb50459d3943bb24859ad004bfdc
vladislav <vladislav.mpm@gmail.com>
2021-11-22 19:05:17 +0100

    modify: change URLs for categories and productLimit

--- src/httpRoutes.js
+++ src/httpRoutes.js
@@ -1,7 +1,7 @@
 const url = {
-  productLimit: 'http://localhost:3000/products?_limit=',
+  productLimit: 'http://localhost:9090/products?_limit=',
   productLimitCategory: '',
-  categories: 'http://localhost:3000/categories',
+  categories: 'http://localhost:9090/product/categories',
   productId: 'http://localhost:3000/products?id=',
   userSimen: 'http://localhost:3000/users/?_fname=Simen',
   storeId: 'http://localhost:3000/store?id=',
