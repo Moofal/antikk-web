d5b79e305afbd31aaf4cb631b01dd64b461fa3e7
vladislav <vladislav.mpm@gmail.com>
2021-11-25 15:14:59 +0100

    modify: api's endpoins

--- src/httpRoutes.js
+++ src/httpRoutes.js
@@ -4,8 +4,8 @@ const url = {
   categories: 'http://localhost:9090/product/categories',
   productId: 'http://localhost:3000/products?id=',
   userSimen: 'http://localhost:3000/users/?_fname=Simen',
-  storeId: 'http://localhost:3000/store?id=',
-  productsStoreId: 'http://localhost:3000/products?storeId=',
+  storeId: 'http://localhost:9090/store/',
+  productsStoreId: 'http://localhost:9090/client/store/',
   orders: 'http://localhost:3000/order',
   putProduct: 'http://localhost:3000/products/',
   orderId: 'http://localhost:3000/order/?id='
