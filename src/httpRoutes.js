91f707eace0b125c5f464d2ec496f6492203ba39
vladislav <vladislav.mpm@gmail.com>
2021-11-25 15:58:51 +0100

    add: getProductDetails()

--- src/httpRoutes.js
+++ src/httpRoutes.js
@@ -1,3 +1,6 @@
+const host = 'http://localhost'
+const port = ':9090'
+
 const url = {
   productLimit: 'http://localhost:9090/products?_limit=',
   productLimitCategory: '',
@@ -13,7 +16,9 @@ const url = {
   addNewProduct: 'http://localhost:9090/business/store/product/add',
   addNewAuction: '#',
 
-  getProductDetails: 'http://localhost:9090/client/product/'
+  getProductDetails: function (storeID) {
+    return host + port + '/client/product/' + storeID + '/details'
+  }
 }
 
 export default url
