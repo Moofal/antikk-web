5bd677a66eefb9de688f4e48907c57b70ee3a398
vladislav <vladislav.mpm@gmail.com>
2021-12-06 19:35:48 +0100

    add: fetch request to add product to the shopping cart

--- src/components/AddToCart.vue
+++ src/components/AddToCart.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="addToCart(product.id)">
+  <button @click="addProdToCart(product.id)">
     Legg i handlevogn
   </button>
 </template>
@@ -7,7 +7,29 @@
 <script>
 export default {
   name: 'AddToCart',
-  props: ['addToCart', 'product']
+  props: ['addToCart', 'product'],
+  methods: {
+    addProdToCart (id) {
+      fetch('http://localhost:9090/client/add-to-cart', {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+        body: JSON.stringify({ products: [id] })
+      })
+        .then(async response => {
+          const data = await response.json()
+          if (!response.ok) {
+            const error = (data && data.message) || response.statusText
+            return Promise.reject(error)
+          }
+          this.addToCart(id)
+        })
+        .catch(error => {
+          this.productErrorMessage = error
+          console.error('There was an error!', error)
+        })
+    }
+  }
 }
 </script>
 
