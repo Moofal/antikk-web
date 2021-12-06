652149e168f8dead467e49189a5c86f2e4786ec0
vladislav <vladislav.mpm@gmail.com>
2021-12-06 19:29:53 +0100

    add: basic auth

--- src/views/LoggInn.vue
+++ src/views/LoggInn.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <button @click="seeAsBusiness">
+<!--    <button @click="seeAsBusiness">-->
+    <button @click="this.SignInBusiness">
       Logg in som Fredrikstad Antikvitet
     </button>
-    <button @click="seeAsEndUser">
+<!--    <button @click="seeAsEndUser">-->
+    <button @click="this.SignInUser">
       Logg in som slutt bruker
     </button>
     <button @click="loggOut">
@@ -15,6 +17,32 @@
 <script>
 export default {
   name: 'LoggInn',
-  props: ['seeAsEndUser', 'seeAsBusiness', 'loggOut']
+  props: ['seeAsEndUser', 'seeAsBusiness', 'loggOut'],
+  methods: {
+    SignInUser () {
+      fetch('http://localhost:9090/user/sign-in', {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include'
+      })
+        .then(async response => {
+          const data = await response.json
+          console.log(data)
+          this.seeAsEndUser()
+        })
+    },
+    SignInBusiness () {
+      fetch('http://localhost:9090/business/sign-in', {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include'
+      })
+        .then(async response => {
+          const data = await response.json
+          console.log(data)
+          this.seeAsBusiness()
+        })
+    }
+  }
 }
 </script>
